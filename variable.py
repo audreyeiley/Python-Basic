8f749daf33442ba4895468fcba6e9cca8598ddc4
Eiley <132795623+audreyeiley@users.noreply.github.com>
2025-01-12 03:05:15 +0000

    20250112

---
+++ variable.py
@@ -0,0 +1,58 @@
+# variable = value
+# Save the value in variable in the format above.
+# = Unlike the meaning of the symbol, the value is simply stored in a variable.
+# Variable can be whatever you want as long as you follow the rules below.
+# 1. Impossible to start with a number
+# 2. Special characters are not possible except the symbal "_"
+# 3. No spacing
+# 4. Reserved words not possible
+
+# my_name = "Eiley"  
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+#  Data types 
+# str   -> String   -> made up of air quotes " " ' '
+# float -> Float    -> no quotes and has a decimal point 
+# int   -> Integer  -> no quotes and no decimal point
+# Everyday words (English, Chinese, korean, etc.) without quotes => error
+
+# a = 10    # saves the integer 10 in the variable a
+# b = 'cit' # saves the string 'cit' in the variable b
+
+# name = 'Eiley'
+# age = 11
+# Height = 150
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# print('text' or variable or number)
+# The print () funtion prints the 'text', number, or value of
+# the variable inside the bracket and then adds a newline.
+# To print mulitple items, use a comma (,)
+# If you print() with nothing inside, it prints an empty line
+
+# name = 'python'
+# age = 11
+# height = 150
+# print(name)
+# print()     # prints an empty line
+# print(age)
+# print(height)
+# print('hi')
+# print()
+# print(5)
+# print(5*10)
+# print(name, age, height, "hello")
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# problem
+# Create two variable(name, school).
+# Make it so that the following content is displayed when run.
+
+name = "Eiley"
+school = 'YISS'
+print("Hello, my name is", name)        # next time add a comma after the " and then write the variable
+print("I am currently a 5th-grade student at", school)
